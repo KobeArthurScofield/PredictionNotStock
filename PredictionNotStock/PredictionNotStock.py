b03a27ee36268752ce590cbe020abdf184771798
KobeArthurScofield <kobearthurxc@outlook.com>
2018-05-24 00:10:15 +0800

    Last Fix

--- PredictionNotStock/PredictionNotStock.py
+++ PredictionNotStock/PredictionNotStock.py
@@ -5,14 +5,8 @@
 # IDE: Microsoft Visual Studio Community 15.7.1
 
 print("Load libraries...")
-import numpy as np
-print("NumPy (1/4) loaded")
-import pandas as pd
-print("Pandas (2/4) loaded")
-from sklearn import datasets, linear_model
-print("Scikit-Learn (3/4) partial loaded")
 import matplotlib.pyplot as mtpl
-print("MatPlotLib.PyPlot (4/4) loaded")
+print("MatPlotLib.PyPlot (1/1) loaded")
 from llib import iodp
 from llib import analyzecore as anlz
 print("MISC Library loaded")
@@ -22,10 +16,15 @@ file = iodp.filepath_trim(input("Please input the path of data: "))
 l_Time, l_Value = iodp.dget_csv_data(file, "Seq", "Lowest")
 a_Time, a_Value = iodp.dget_csv_data(file, "Seq", "Average")
 
-l_xi, l_predict = anlz.get_data(l_Time, l_Value)
-a_xi, a_predict = anlz.get_data(a_Time, a_Value)
+# l_xi, l_predict = anlz.get_data(l_Time, l_Value)
+# a_xi, a_predict = anlz.get_data(a_Time, a_Value)
 
-print(l_xi, l_predict, a_xi, a_predict)
+# print(l_Time, l_Value, a_Time, a_Value)
+
+low_predict = anlz.predict_value(l_Time, l_Value, (len(l_Time) + 1))
+avr_predict = anlz.predict_value(a_Time, a_Value, (len(a_Time) + 1))
+
+print("Lowest prediction: %f, Average predition: %f" % (float(low_predict), float(avr_predict)))
 
 mtpl.figure(figsize=(6, 4), dpi=300)
 mtpl.plot(l_Time, l_Value, label="Lowest")
